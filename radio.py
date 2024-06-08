8778f4a840c30d5863933e10eba62319af43c674
iUnstable0 <me@iunstable0.com>
2024-06-09 06:21:19 +0700

    FIXED :D

--- radio.py
+++ radio.py
@@ -17,11 +17,11 @@ def cb(events):
     pass
 
 sx = SX1262(spi_bus=1, clk=10, mosi=11, miso=12, cs=3, irq=20, rst=15, gpio=2)
-sx.begin(freq=923, bw=500.0, sf=12, cr=8, syncWord=0x12,
+sx.begin(freq=902.0, bw=500.0, sf=12, cr=8, syncWord=0x12,
          power=-5, currentLimit=60.0, preambleLength=8,
          implicit=False, implicitLen=0xFF,
          crcOn=True, txIq=False, rxIq=False,
-         tcxoVoltage=1.7, useRegulatorLDO=False, blocking=True)
+         tcxoVoltage=0, useRegulatorLDO=False, blocking=True)
 sx.setBlockingCallback(False, cb)
 
 def sendMSG(msg, recp):
