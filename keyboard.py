e2d8bac451130658dcfc5b2d94ce349ffb92219b
evan-gan <139721374+evan-gan@users.noreply.github.com>
2024-07-04 22:06:25 -0400

    Starting to finish

--- keyboard.py
+++ keyboard.py
@@ -1,33 +1,37 @@
 from machine import Pin
+# import uasyncio
 import utime
+
 import CONSTS
 
-COL_PINS = [10, 27, 6]
-ROW_PINS = [14, 13, 21]
 
 class Keyboard:
-    #Format is ROWCOL : [key value, key value if shifted]
-    KEYS = {
-        #Note: Most of row 1 is non typing keys
-        '11': [" "," "], 
-        '28': ["/","?"],
-        '27': [".",">"],
-        '26': [",","<"],
-        '25': ["m","M"],
-        '24': ["n","N"],
-        '23': ["b","B"],
-        '22': ["v","V"],
-        '21': ["c","C"],
-        '31': ["j","J"],
-        '32': ["k","K"],
-        '33': ["l","L"],
-        '34': [";",":"],
-        '35': ["'",'"'],
-        '36': ["\n","\n"], #ENTER KEY
-        '37': ["z","Z"],
-        '28': ["nonSHIFT","SHIFT"],
-        'ROWCOL': ["nonSHIFT","SHIFT"],}
-    KEYS2 = {
+    # Format is ROWCOL : [key value, key value if shifted]
+    KEYS:dict[str, list[str]] = {
+        # Note: Most of Col 1 is non typing keys
+        '11': [" ", " "],
+        # '12' - LEFT
+        # '13' - UP
+        # '14' - RIGHT
+        # '15' - DOWN
+        '16': [" ", " "],
+        '17': [" ", " "],
+        '18': [" ", " "],
+        '28': ["/", "?"],
+        '27': [".", ">"],
+        '26': [",", "<"],
+        '25': ["m", "M"],
+        '24': ["n", "N"],
+        '23': ["b", "B"],
+        '22': ["v", "V"],
+        '21': ["c", "C"],
+        '31': ["j", "J"],
+        '32': ["k", "K"],
+        '33': ["l", "L"],
+        '34': [";", ":"],
+        '35': ["'", '"'],
+        '37': ["z", "Z"],
+        '38': ["x", "X"],
         "71": ["1", "!"],
         "72": ["2", "@"],
         "73": ["3", "#"],
@@ -39,7 +43,7 @@ class Keyboard:
         "61": ["9", "("],
         "62": ["0", ")"],
         "63": ["-", "_"],
-        "64": ["=", "+"], # 65 is backspace
+        "64": ["=", "+"],
         "66": ["q", "Q"],
         "67": ["w", "W"],
         "68": ["e", "E"],
@@ -52,39 +56,150 @@ class Keyboard:
         "57": ["p", "P"],
         "58": ["[", "{"],
         "41": ["]", "}"],
+        "42": ["\\", "|"],
+        "43": ["a", "A"],
+        "44": ["s", "S"],
+        "45": ["d", "D"],
+        "46": ["f", "F"],
+        "47": ["g", "G"],
+        "48": ["h", "H"]
     }
 
-    def __init__(self, sendMSG):
-        for pin in COL_PINS:
-            Pin(pin, Pin.IN, Pin.PULL_UP)
+    # actionKeys = {
+    #     "BACKSPACE": "65",
+    #     "ENTER": "36",
 
-        for pin in ROW_PINS:
-            Pin(pin, Pin.IN, Pin.PULL_UP)
-        
+    #     "LEFT": "12",
+    #     "UP": "13",
+    #     "RIGHT": "14",
+    #     "DOWN": "15"
+    # }
+
+    actionKeys = {
+        "65": "Backspace",
+        "36": "Enter",
+        "12": "Left",
+        "13": "Up",
+        "14": "Right",
+        "15": "Down",
+    }
+
+    pull_downs = {
+        "Col1": 6,
+        "Col2": 7,
+        "Col3": 8,
+        "Col4": 9,
+        "Col5": 13,
+        "Col6": 14,
+        "Col7": 5,
+        "Col8": 4,
+
+        "Row1": 27,
+        "Row2": 26,
+        "Row3": 22,
+        "Row4": 21,
+        "Row5": 19,
+        "Row6": 18,
+        "Row7": 17,
+    }
+
+    pull_ups = {
+        "SHIFT": 28
+    }
+
+    keys_cooldown = {}
+
+    def __init__(self, onKeyPress, onLeft, onUp, onRight, onDown):
+        self.onKeyPress = onKeyPress
+
+        self.onLeft = onLeft
+        self.onUp = onUp
+        self.onRight = onRight
+        self.onDown = onDown
+
+        self.focused_element = None
+
+        for key, value in self.pull_downs.items():
+            setattr(self, key, Pin(value, Pin.IN, Pin.PULL_DOWN))
+
+        for key, value in self.pull_ups.items():
+            setattr(self, key, Pin(value, Pin.IN, Pin.PULL_UP))
+
+        # Set up the interrupts on the rising edge (button press) for each pin
+        for key, value in self.pull_downs.items():
+            getattr(self, key).irq(trigger=Pin.IRQ_RISING,
+                                   handler=self.handleKeyPress)
+
         self.SHIFT_PIN = Pin(28, Pin.IN, Pin.PULL_UP)
-        self.COL_GS_PIN = Pin(7, Pin.IN)
-        self.ROW_GS_PIN = Pin(9, Pin.IN, Pin.PULL_UP)
-        
+
         self.last_press_time = 0
-        self.last_press_time_d = 0
 
-        self.sendMSG = sendMSG
-
-        Pin(COL_PINS[0], Pin.IN, Pin.PULL_UP).irq(trigger=Pin.IRQ_FALLING, handler=self.keypor)
-        self.COL_GS_PIN.irq(trigger=Pin.IRQ_FALLING, handler=self.keypor)
-        self.SHIFT_PIN.irq(trigger=Pin.IRQ_FALLING, handler=self.shift_pressed)
-
-
-    def shift_pressed(self, pin):
+    def checkKeyCooldown(self, RowCol):
         current_time = utime.ticks_ms()
-        # Check if the debounce time (200 ms) has passed
-        if utime.ticks_diff(current_time, self.last_press_time) > CONSTS.DEBOUNCE_TIME:
-            self.sendMSG("hello world!")
-            self.last_press_time = current_time
-    
-    def keypor(self, pin):
-        current_time = utime.ticks_ms()
-        # Check if the debounce time (200 ms) has passed
-        if utime.ticks_diff(current_time, self.last_press_time_d) > CONSTS.DEBOUNCE_TIME:
-            print("Key pressed", current_time)
-            self.last_press_time_d = current_time
+
+        return utime.ticks_diff(current_time, self.keys_cooldown.get(RowCol, CONSTS.KEYBOARD_DEBOUNCE_TIME_MS + 1)) > CONSTS.KEYBOARD_DEBOUNCE_TIME_MS
+
+    def handleKeyPress(self, pin):
+        # print("Called!", pin)
+
+        RowCol = self.getRowPressed() + self.getColPressed()
+
+        if "0" in RowCol:
+            return
+
+        # Check if the debounce time (150 ms) has passed, if it has not return
+        if self.checkKeyCooldown(RowCol):
+            self.keys_cooldown[RowCol] = utime.ticks_ms()
+        else:
+            return
+
+        # print(f"{RowCol} was pressed!")
+
+        action = self.actionKeys.get(RowCol)
+
+        if action:
+            if self.focused_element:
+                if action == "Backspace":
+                    if hasattr(self.focused_element, "text"):
+                        self.focused_element.text = self.focused_element.text[:-1]
+                elif action == "Enter":
+                    if hasattr(self.focused_element, "onEnter"):
+                        print("Call onEnter!")
+                        self.focused_element.onEnter(self.focused_element)
+            else:
+                # TODO: Lol
+                # getattr(self, f"on{action}")()
+                pass
+
+            return
+
+        key = self.KEYS.get(RowCol)
+
+        if key:
+            if self.focused_element:
+                self.focused_element.text += key[1 if self.SHIFT_PIN.value()
+                                                 == 0 else 0]
+            # else:
+            #     self.onKeyPress(
+            #         key[1 if self.SHIFT_PIN.value() == 0 else 0])
+
+    def getColPressed(self):
+        cols = {k: v for k, v in self.pull_downs.items() if k.startswith("Col")}
+
+        for col in cols:
+            if getattr(self, col).value() == 1:
+                return col.split("Col")[1]
+
+        return "0"
+
+    def getRowPressed(self):
+        rows = {k: v for k, v in self.pull_downs.items() if k.startswith("Row")}
+
+        for row in rows:
+            if getattr(self, row).value() == 1:
+                return row.split("Row")[1]
+
+        return "0"
+
+    def setFocus(self, element):
+        self.focused_element = element
