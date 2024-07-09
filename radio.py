187e3813ebaff9e8c93f9db34a7737f6946c9355
evan-gan <139721374+evan-gan@users.noreply.github.com>
2024-07-09 10:29:15 -0400

    New lora settings

--- radio.py
+++ radio.py
@@ -3,11 +3,32 @@ from lib.sx1262 import SX1262
 class Radio:
     def __init__(self, receivedMSG):
         self.sx = SX1262(spi_bus=1, clk=10, mosi=11, miso=12, cs=3, irq=20, rst=15, gpio=2)
-        self.sx.begin(freq=902.0, bw=500.0, sf=12, cr=8, syncWord=0x12,
-            power=-5, currentLimit=60.0, preambleLength=8,
-            implicit=False, implicitLen=0xFF,
-            crcOn=True, txIq=False, rxIq=False,
-            tcxoVoltage=0, useRegulatorLDO=False, blocking=True)
+        # self.sx.begin(freq=902.0, bw=500.0, sf=12, cr=8, syncWord=0x12,
+        #     power=-5, currentLimit=60.0, preambleLength=8,
+        #     implicit=False, implicitLen=0xFF,
+        #     crcOn=True, txIq=False, rxIq=False,
+        #     tcxoVoltage=0, useRegulatorLDO=False, blocking=True)
+
+        #New test settings
+        self.sx.begin(
+        freq=902.0, 
+        bw=125.0,       # New value
+        sf=10,          # New value
+        cr=8, 
+        syncWord=0x34,  # New value
+        power=14,       # New value
+        currentLimit=60.0, 
+        preambleLength=12,  # New value
+        implicit=False, 
+        implicitLen=0xFF,
+        crcOn=True, 
+        txIq=False, 
+        rxIq=False,
+        tcxoVoltage=0, 
+        useRegulatorLDO=False, 
+        blocking=True
+        )
+
         self.receivedMSG = receivedMSG
         self.sx.setBlockingCallback(False, self.cb)
     
